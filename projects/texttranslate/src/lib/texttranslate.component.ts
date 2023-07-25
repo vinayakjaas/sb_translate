35487c6a79188430b6e881c9839d6676741fa6a3
Vinayak Raj Ranjan <vinayakrajqaz@gmail.com>
2023-07-25 20:12:19 +0530

    Some Update

--- projects/texttranslate/src/lib/texttranslate.component.ts
+++ projects/texttranslate/src/lib/texttranslate.component.ts
@@ -110,9 +110,7 @@ export class TexttranslateComponent  implements AfterContentInit{
     if (!this.contentElementRef) return;
 
     const url = 'https://dhruva-api.bhashini.gov.in/services/inference/pipeline';
-    // const userId = '26f83e9d425a40a28ec59f944cb4da6c';
-    // const apiKey = '1829da2d40-6a44-4d3d-ac61-9850afd41175';
-    // const authorizationToken = 'cYZieH0OCn8PUZbylALnCkwvs8PYvAiYwiQ7FUj1V7_Vspia5jvxFs0T3R3-DXF_';
+    
 
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
