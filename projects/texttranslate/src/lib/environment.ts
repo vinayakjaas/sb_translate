dadc82e4bb64cdee9bba197aac92998269ef8457
Vinayak Raj Ranjan <vinayakrajqaz@gmail.com>
2023-07-29 15:44:49 +0530

    Update

--- projects/texttranslate/src/lib/environment.ts
+++ projects/texttranslate/src/lib/environment.ts
@@ -1,9 +1,4 @@
-// export const environment = {
-//   production: false,
-//   apiKey: '1829da2d40-6a44-4d3d-ac61-9850afd41175',
-//   userId: '26f83e9d425a40a28ec59f944cb4da6c',
-//   authorizationToken: 'cYZieH0OCn8PUZbylALnCkwvs8PYvAiYwiQ7FUj1V7_Vspia5jvxFs0T3R3-DXF_',
-// };
+
 export const environment = {
   production: false,
   apiKey: 'YOUR_API_KEY',
