652e9ab5a27248d0f826125529b4b5dd22158f15
Vinayak Raj Ranjan <96948435+vinayakjaas@users.noreply.github.com>
2023-08-31 00:30:42 +0530

    Update environment.ts

--- projects/texttranslate/src/lib/environment.ts
+++ projects/texttranslate/src/lib/environment.ts
@@ -1,7 +1,6 @@
-
 export const environment = {
   production: false,
-  apiKey: 'YOUR_API_KEY',
-  userId: 'YOUR_USER_ID',
-  authorizationToken: 'YOUR_AUTH_TOKEN'
+  apiKey: '1829da2d40-6a44-4d3d-ac61-9850afd41175',
+  userId: '26f83e9d425a40a28ec59f944cb4da6c',
+  authorizationToken: 'cYZieH0OCn8PUZbylALnCkwvs8PYvAiYwiQ7FUj1V7_Vspia5jvxFs0T3R3-DXF_'
 };
